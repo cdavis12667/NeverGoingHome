3a19ef404f1e7842f5a28394f80626db96d549a1
cdavis12667 <cdavis13889@gmail.com>
2024-03-19 11:55:39 -0500

    Add skills , abilities and whispers to npc sheet

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -4,8 +4,8 @@ export class NevergoinghomeActorSheet extends ActorSheet {
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ['nevergoinghome', 'sheet', 'actor'],
-      width: 600,
-      height: 600,
+      width: 800,
+      height: 800,
     });
   }
 //get the tempplate from my actor sheet
